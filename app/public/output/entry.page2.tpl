d600eed73f5900699c598471b174899b7b273895
18352978082 <1091123779@qq.com>
2025-11-08 13:08:33 +0800

    feat: 修改报错

--- app/public/output/entry.page2.tpl
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html >
-<head>
-    <title>Document</title>
-</head>
-<body style="color:blue">
-    <h1>Page2</h1>
-</body>
-</html>
--- app/router/view.js
+++ app/router/view.js
@@ -1,7 +1,7 @@
 module.exports = (app, router) => {
     const { view: ViewController } = app.controller
-    console.log(ViewController.renderPage,8888);    
-     console.log(ViewController.renderPage.bind(ViewController));  
+    // console.log(ViewController.renderPage,8888);    
+    //  console.log(ViewController.renderPage.bind(ViewController));  
     //用户输入 http://ip:port/view/xxxx 就能渲染出对应的页面 
     router.get('/view/:page',ViewController.renderPage.bind(ViewController))
      
