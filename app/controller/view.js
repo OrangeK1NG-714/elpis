88e5ec02769c26881efbe5be543aa176b1b09085
18352978082 <1091123779@qq.com>
2025-11-08 13:10:03 +0800

    Revert "feat: 修改报错"
    This reverts commit d600eed73f5900699c598471b174899b7b273895.

--- app/controller/view.js
+++ app/controller/view.js
@@ -5,11 +5,7 @@ module.exports =(app)=>{
          * @param {Object} ctx 上下文 
          */
         async renderPage(ctx){
-            await ctx.render(`dist/entry.${ctx.params.page}`,{
-                name:app.options?.name,
-                env:app.env.get(),
-                options:JSON.stringify(app.options)
-            })
+            await ctx.render(`output/entry.${ctx.params.page}`)
         }
     }
 }
